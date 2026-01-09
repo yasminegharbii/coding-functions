f8e71c2bfdcf53a323632f9bc8f05528f7500ecd
yasminegharbii <jasminenotdead@gmail.com>
2026-01-09 23:59:59 +0100

    myhomework

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
 
+// ex 1
+
+function myFirstFunction() {
+
+}
+myFirstFunction();
+
+// ex 2
+
+function helloWorld() {
+    console.log("hello World");
+
+}
+helloWorld();
+
+// ex 3
+
+function returnFive() {
+    return 5;
+}
+returnFive();
+
+// ex 4
+
+function helloWorldAgain() {
+    return ("hello World Again");
+}
+helloWorldAgain();
+
+// ex 5
+
+function returnParameter(parameter) {
+    return parameter; }
+    console.log(returnParameter(1));
+    console.log(returnParameter(2));
+    console.log(returnParameter("Hi"));
+
+    // ex 6
+
+  function add(firstNumber,secondNumber) {
+    return firstNumber+secondNumber;
+  }  
+  console.log(add(1, 1));
+  console.log(add(2, 2));
+
+  // ex 7
+
+  function subtract(firstNumber, secondNumber) {
+    return firstNumber-secondNumber;
+  }
+console.log(subtract(2, 1));
+console.log(subtract(3, 3));
+console.log(subtract(1, 2));
+
+// ex 8
+
+function multiply(FirstNumber,SecondNumber) {
+   return FirstNumber*SecondNumber
+}
+console.log(multiply(2, 3));
+console.log(multiply(4, 5));
+
+// ex 9
+
+function stringLength(string) {
+    return string.length;
+}
+console.log(stringLength("Hello"));
+console.log(stringLength("Hi"));
+ // ex 10 
+
+function concatTwoStrings(firstString,secondString) {
+    return firstString+secondString;
+}
+console.log(concatTwoStrings("Hello","World"));
+console.log(concatTwoStrings("Cute","Kitty"));
+
+
+
